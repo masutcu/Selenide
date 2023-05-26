f9392ba68b056f9104e8f1c0936684172eb1d821
masutcu <masutcu@gmail.com>
2023-05-26 22:57:25 +0300

    start

---
+++ src/test/java/runner/Runners.java
@@ -0,0 +1,27 @@
+package runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {"html:target/cucumber-report_smoke.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "pretty",
+        },
+        monochrome = true,
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "@google",
+        dryRun = false
+
+
+)
+
+
+public class Runners {
+}
---
+++ src/test/java/utilities/Reusable.java
@@ -0,0 +1,8 @@
+package utilities;
+
+public class Reusable {
+
+    public void bekle(int saniye) throws InterruptedException {
+        Thread.sleep(saniye*1000);
+    }
+}
